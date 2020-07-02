ba95fd67337e69d81ff53f2121e84ed74e6ff533
VlasenkoYuliia <ogirenkojulia@gmail.com>
2020-07-02 14:33:45 +0300

    Commit 4: auth failed exception

---
+++ src/main/java/com/yuliia/vlasenko/imagesearcher/api/auth/AuthException.java
@@ -0,0 +1,7 @@
+package com.yuliia.vlasenko.imagesearcher.api.auth;
+
+public class AuthException extends RuntimeException {
+    public AuthException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/yuliia/vlasenko/imagesearcher/api/auth/AuthService.java
+++ src/main/java/com/yuliia/vlasenko/imagesearcher/api/auth/AuthService.java
@@ -1,11 +1,13 @@
 package com.yuliia.vlasenko.imagesearcher.api.auth;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class AuthService {
     private final RestTemplate restTemplate;
     @Value("${imageapi.key}")
@@ -18,11 +20,13 @@ public class AuthService {
     }
 
     public void authenticate() {
+        log.info("Authenticate token request...");
         AuthRequest authRequest = new AuthRequest(apiKey);
         AuthResponse authResponse = restTemplate.postForObject("http://interview.agileengine.com/auth", authRequest, AuthResponse.class);
-        if (authResponse != null) {
-            this.token = authResponse.getToken();
+        if (authResponse == null || !authResponse.isAuth()) {
+            throw new AuthException("Auth request failed");
         }
+        this.token = authResponse.getToken();
     }
 
     public String getToken() {
